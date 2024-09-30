94088509f47ac15edc2d932528018160a49c3896
Minh Trong <traminhtrong2604@gmail.com>
2024-09-30 14:40:42 +0700

    test: adding topic for peers to exchange

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-import { keys } from '@libp2p/crypto'
+import { keys, randomBytes } from '@libp2p/crypto'
 
 import { PRIVATE_KEY } from './config'
+import Swarm from './swarm'
 
-const privKey = keys.privateKeyFromRaw(Buffer.from(PRIVATE_KEY, 'hex'))
+async function main() {
+    const privKey = keys.privateKeyFromRaw(Buffer.from(PRIVATE_KEY, 'hex'))
+    const { swarm } = await Swarm.new(privKey)
+    await swarm.services.dht.setMode('server')
+    await swarm.start()
 
-console.log(privKey)
+    const topic = 'randao'
+    swarm.services.pubsub.subscribe(topic)
+    swarm.services.pubsub.addEventListener('message', ({ detail }) => {
+        console.log(
+            'Message:',
+            detail.topic,
+            Buffer.from(detail.data).toString('hex'),
+        )
+    })
+
+    setInterval(async () => {
+        const ok = swarm.services.pubsub.getSubscribers(topic).length
+        if (ok) swarm.services.pubsub.publish(topic, randomBytes(32))
+    }, Math.ceil(10000 * Math.random()))
+}
+
+main()
